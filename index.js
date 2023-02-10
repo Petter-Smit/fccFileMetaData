669119a7e6c0bd2a91ab5f2d38d8cebe90261c14
CrushererBrooks <PetterESmit@outlook.com>
2023-02-10 15:17:34 -0300

    Feat: local upload of files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
+
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single("upfile"), (req, res) => {
+  res.json({name: req.file.originalname, size: req.file.size, type: req.file.mimetype});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
